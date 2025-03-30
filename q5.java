8118913638cc6861478ed5aa179459ebdf885673
Gowtham T <gowtham.t20062@gmail.com>
2025-03-30 19:32:22 +0530

    Nested if

---
+++ q5.java
@@ -0,0 +1,29 @@
+class q5{
+    public static void main(String args[]){
+        boolean kfc = true;
+        boolean chicken = false;
+        boolean pepsi = false;
+
+        if(kfc){
+            System.out.print("Enter into KFC");
+
+            if(chicken){
+                System.out.println("eating chicken");
+
+                if(pepsi){
+                    System.out.print("Drinking pepsi");
+                }
+                else{
+                    System.out.println("Orderr next item");
+                }
+            }
+            else{
+                System.out.println("Eat Burger");
+            }
+
+
+
+
+        }
+    }
+}
